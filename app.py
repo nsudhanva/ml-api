04f871bd3936a0cf936d1709802d15f22bf6668d
Sudhanva <nsudhanva@gmail.com>
2018-08-17 19:16:55 +0530

    Load model for every request

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import json
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    implicit_model = joblib.load('./model/user_submissions_model.pkl')
-    user_assignments_sparse = sparse.load_npz('./model/user_assignments.npz')
     app.run()
 
 @app.route("/")
@@ -18,6 +16,8 @@ def hello():
 @app.route('/recommend', methods=['GET'])
 
 def recommend():
+    implicit_model = joblib.load('./model/user_submissions_model.pkl')
+    user_assignments_sparse = sparse.load_npz('./model/user_assignments.npz')
     user_id = request.args.get('user_id')
     recommendations = implicit_model.recommend(int(user_id), user_assignments_sparse, N=20)
     recommendations = dict(recommendations)
@@ -27,6 +27,8 @@ def recommend():
 @app.route('/related', methods=['GET'])
 
 def related():
+    implicit_model = joblib.load('./model/user_submissions_model.pkl')
+    user_assignments_sparse = sparse.load_npz('./model/user_assignments.npz')
     submission_id = request.args.get('submission_id')
     related = implicit_model.similar_items(int(submission_id), N=20)
     related = dict(related)
